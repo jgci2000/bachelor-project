8e6151a55976e706912e495c94d5686a77dfed6c
João Inácio <jgci2000@hotmail.com>
2021-03-25 12:49:16 +0000

    added Ising class

---
+++ Flat Scan Sampling/src_Ising/CPP new/Ising.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "Ising.h"
+
+
+
+
---
+++ Flat Scan Sampling/src_Ising/CPP new/Ising.h
@@ -0,0 +1,14 @@
+#ifndef ISING_H
+#define ISING_H
+
+class Ising {
+    private:
+
+    public:
+        Ising()
+        {
+            
+        }
+};
+
+#endif // ISING_H
