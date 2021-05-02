23b59fd6198cd3a55750e02c3af3a26413af21a8
João Inácio <jgci2000@hotmail.com>
2021-05-02 16:17:51 +0100

    converted some normalization coefs

--- lattices_and_factors/normalization_coefs/mat/mat_to_txt.m
+++ lattices_and_factors/normalization_coefs/mat/mat_to_txt.m
@@ -4,8 +4,8 @@ close
 % A simple script that converts the NN table from .mat to .txt
 % João Inácio, Oct. 7, 2020
 
-N_vals = [4, 8, 9, 16, 27, 32, 54, 64, 108, 128, 256, 512, 1024];
-S_vals = [11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 8, 2];
+N_vals = [4, 8, 9, 16, 27, 32, 54, 64];%, 108, 128, 256, 512, 1024];
+S_vals = [11, 11, 11, 11, 11, 11, 11, 11];% 10, 11, 11, 8, 2];
 
 for idx = 1:length(N_vals)
     N = N_vals(idx);
@@ -18,7 +18,7 @@ for idx = 1:length(N_vals)
         
         load("coefficients_" + N + "d" + S + ".mat")
 
-        writematrix(norm_factor, "txt/coefficients_" + N + "d" + S + ".txt")
+        writematrix(norm_factor, "../txt/coefficients_" + N + "d" + S + ".txt")
 %         if (N == 32 && S >= 4) || N > 32
 %             writematrix(norm_factor_log, "txt/coefficients_log_" + N + "d" + S + ".txt")
 %         end
