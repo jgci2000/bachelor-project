f998a19b12cb49d33654552d07090c02e7f1a194
João Inácio <jgci2000@hotmail.com>
2021-06-30 17:50:37 +0100

    new normalization coefficient

--- compute_versions/src/wl_Ising.cpp
+++ compute_versions/src/wl_Ising.cpp
@@ -38,9 +38,9 @@ using std::to_string;
 #define SEED        0
 
 // Size of the Ising Lattice
-#define L_LATTICE   4
+#define L_LATTICE   8
 // LATTICE_NUM -> 1 - SS; 2 - SC; 3 - BCC; 4 - FCC; 5 - HCP; 6 - Hex 
-#define LATTICE_NUM 1
+#define LATTICE_NUM 2
 
 // Output location
 #define SAVE_DIR(lattice, L, f_final)    "./data/wl_Ising/" + lattice + "/L" + to_string(L) + "/" + to_string(f_final) + "/"
